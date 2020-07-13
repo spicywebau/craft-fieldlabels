b2981524c622045d84d8e1f6186fe431d44b90a0
Thomas Templeton <thomas.templeton.dev@gmail.com>
2020-07-13 14:30:49 +1000

    Prevent the plugin from executing on Craft 3.5

--- src/Plugin.php
+++ src/Plugin.php
@@ -59,6 +59,12 @@ class Plugin extends BasePlugin
     {
         parent::init();
 
+        // Craft 3.5 has native field relabelling, Field Labels is no longer necessary
+        // TODO: add migration and deprecation notice
+        if (class_exists('craft\base\FieldLayoutElement')) {
+            return;
+        }
+
         self::$plugin = $this;
 
         $this->setComponents([
